b49b8e355ff6b052cf2b26d473766c13344d3211
Shouq <shouq.thwap@gmail.com>
2024-01-31 11:28:58 +0300

    System.out.println("hello github")

---
+++ src/lab2_github251/Lab2_GITHUB251.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_github251;
+
+/**
+ *
+ * @author 2206838
+ */
+public class Lab2_GITHUB251 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello Github");
+    }
+    
+}
